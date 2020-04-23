f56c9bafb9886f01318892925e28e4b4d2bee912
David Novak <davidnovak252@gmail.com>
2020-04-23 15:34:13 +0200

    Task 3 commit

--- LV1/Program.cs
+++ LV1/Program.cs
@@ -6,7 +6,14 @@ namespace LV1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Record record;
+            Record record1= new Record("Text1", "Marko", 2);
+            Record record2 = new Record("Text2", 4);
+            Record record3 = new Record(3,"Ivo");
+
+            Console.WriteLine(record1.getText() + ", " + record1.getAuthor() + ", " + record1.getImportance());
+            Console.WriteLine(record2.getText() + ", " + record2.getImportance());
+            Console.WriteLine(record3.getImportance() + ", " + record3.getAuthor());
         }
     }
 }
--- LV1/Record.cs
+++ LV1/Record.cs
@@ -9,11 +9,29 @@ namespace LV1
         private String text;
         private String author;
         private int importance;
-
-        public String getText() { return this.text; }
-        public String getAuthor() { return this.author; }
+        
+        public string getText() { return this.text; }
+        public string getAuthor() { return this.author; }
         public int getImportance() { return this.importance; }
-        public void setText(String text) { this.text = text; }
+        public void setText(string text) { this.text = text; }
         public void setImportance(int importance) { this.importance = importance; }
+
+        public Record() { }            
+        public Record (string author){
+            this.author = author;
+        }
+        public Record(string text, string author, int importance){
+            this.text = text;
+            this.author = author;
+            this.importance = importance;
+        }
+        public Record(string text, int importance){
+            this.text = text;
+            this.importance = importance;
+        }
+        public Record(int importance, string author){
+            this.importance = importance;
+            this.author = author;
+        }
     }
 }
