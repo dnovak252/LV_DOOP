646acfa06da430e59d88912a496c7c82d372e059
David Novak <davidnovak252@gmail.com>
2020-04-23 14:46:50 +0200

    Task 1 commit

---
+++ LV1/Record.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV1
+{
+    class Record
+    {
+        private String text;
+        private String author;
+        private int importance;
+
+        private String getText() { return this.text; }
+        private String getAuthor() { return this.author; }
+        private int getImportance() { return this.importance; }
+        private void setText(String text) { this.text = text; }
+        private void setImportance(int importance) { this.importance = importance; }
+    }
+}
