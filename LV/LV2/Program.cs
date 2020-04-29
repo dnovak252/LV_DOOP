82b9c35695362cdf26639f53c6d80c4beaa15de6
David Novak <davidnovak252@gmail.com>
2020-04-29 12:37:38 +0200

    Task1 LV2

---
+++ LV/LV2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceRoller diceRoller = new DiceRoller();
+            int numberOfObjects = 20;
+            for (int i=0;i<numberOfObjects; i++)
+            {
+                diceRoller.InsertDie(new Die(6));
+            }
+            diceRoller.RollAllDice();
+            diceRoller.rollingPrint(diceRoller.GetRollingResults());
+        }
+    }
+}
