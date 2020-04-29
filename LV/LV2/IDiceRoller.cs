38fbb9eb97ec076542fd9b0aebb598c7394dd57d
David Novak <davidnovak252@gmail.com>
2020-04-29 15:46:39 +0200

    Task6 LV2

---
+++ LV/LV2/IDiceRoller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV2
+{
+    interface IDiceRoller
+    {
+        void InsertDie();
+        void RemoveAllDice();
+        void RollAllDice();
+    }
+}
---
+++ LV/LV2/IFlexibleDiceRoller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV2
+{
+    interface IFlexibleDiceRoller
+    {
+        void RemoveAllDice();
+        public void RollAllDice();
+    }
+}
