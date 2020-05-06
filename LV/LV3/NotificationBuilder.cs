ffc9b6697b29368241090a23928d6642a1c6229c
David Novak <davidnovak252@gmail.com>
2020-05-06 22:03:36 +0200

    Task5 LV3 Commit

---
+++ LV/LV3/NotificationBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV3
+{
+    class NotificationBuilder : IBuilder
+    {
+        private string author, title, text;
+        private DateTime time;
+        private ConsoleColor color;
+        private Category level;
+        public ConsoleNotification Build()
+        {
+            return new ConsoleNotification(author, title, text, time, level, color);
+        }
+        public IBuilder SetAuthor(String author)
+        {
+            this.author = author;
+            return this;
+        }
+        public IBuilder SetTitle(String title)
+        {
+            this.title = title;
+            return this;
+        }
+        public IBuilder SetText(String text)
+        {
+            this.text = text;
+            return this;
+        }
+        public IBuilder SetTime(DateTime time)
+        {
+            this.time = time;
+            return this;
+        }
+        public IBuilder SetLevel(Category level)
+        {
+            this.level = level;
+            return this;
+        }
+        public IBuilder SetColor(ConsoleColor color)
+        {
+            this.color = color;
+            return this;
+        }
+        
+    }
+}
