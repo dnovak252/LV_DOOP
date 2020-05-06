e44a399923f02b889c1419aef55863e617634ecd
David Novak <davidnovak252@gmail.com>
2020-05-06 21:23:04 +0200

    Task3 LV3 Commit

---
+++ LV/LV3/FileLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LV3
+{
+    class FileLogger
+    {
+        private static FileLogger instance;
+        private string filePath;
+        public FileLogger(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public static FileLogger GetInstance(string filePath)
+        {
+            if (instance == null)
+            {
+                instance = new FileLogger(filePath);
+            }
+            return instance;
+        }
+        public void Log(string note)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(filePath, true))
+            {
+                writer.WriteLine(note);
+            }
+        }
+    }
+}
