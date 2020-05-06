97ccb58df81d1abc3e1ca3fd5b3e3bfa00fe6c27
David Novak <davidnovak252@gmail.com>
2020-05-06 22:15:39 +0200

    Task6 LV3 Commit

---
+++ LV/LV3/DirectorClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV3
+{
+    class DirectorClass
+    {
+        public void INFOConsoleNotification(IBuilder builder, string author)
+        {
+            builder.SetAuthor(author).SetTitle("Title").SetText("Test Text").SetTime(DateTime.Now).SetLevel(Category.INFO).SetColor(ConsoleColor.Green);
+        }
+        public void ALERTConsoleNotification(IBuilder builder, string author)
+        {
+            builder.SetAuthor(author).SetTitle("Title").SetText("Test Text").SetTime(DateTime.Now).SetLevel(Category.INFO).SetColor(ConsoleColor.Yellow);
+        }
+        public void ERRORConsoleNotification(IBuilder builder, string author)
+        {
+            builder.SetAuthor(author).SetTitle("Title").SetText("Test Text").SetTime(DateTime.Now).SetLevel(Category.INFO).SetColor(ConsoleColor.Red);
+        }
+    }
+}
