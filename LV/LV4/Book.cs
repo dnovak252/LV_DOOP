67b41880f235f7305e1737b222f463dbc573a8fc
David Novak <davidnovak252@gmail.com>
2020-05-14 18:26:23 +0200

    Task3 LV4 Commit

---
+++ LV/LV4/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV4
+{
+    class Book : IRentable
+    {
+        private readonly double BaseBookPrice = 3.99;
+        public String Name { get; private set; }
+        public Book(String name) { this.Name = name; }
+        public string Description { get { return this.Name; } }
+        public double CalculatePrice()
+        {
+            return BaseBookPrice;
+        }
+    }
+}
--- LV/LV4/Program.cs
+++ LV/LV4/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace LV4
 {
@@ -8,24 +8,33 @@ namespace LV4
         static void Main(string[] args)
         {
             //2.zadatak
-            Dataset dataset = new Dataset();
-            dataset.LoadDataFromCSV(@"D:/LV_DOOP/LV/LV4/test.csv");
-            Analyzer3rdParty analyzer = new Analyzer3rdParty();
-            Adapter adapter = new Adapter(analyzer);
-            double[] row = new double[4];
-            double[] column = new double[4];
-            row = adapter.CalculateAveragePerRow(dataset);
-            column = adapter.CalculateAveragePerColumn(dataset);
-            for (int i = 0; i < row.Length; i++)
-            {
-                Console.WriteLine(row[i] + "  ");
-            }
-            Console.WriteLine("\n");
-            for (int i = 0; i < column.Length; i++)
-            {
-                Console.WriteLine(column[i] + "  ");
-            }
+            //Dataset dataset = new Dataset();
+            //dataset.LoadDataFromCSV(@"D:/LV_DOOP/LV/LV4/test.csv");
+            //Analyzer3rdParty analyzer = new Analyzer3rdParty();
+            //Adapter adapter = new Adapter(analyzer);
+            //double[] row = new double[4];
+            //double[] column = new double[4];
+            //row = adapter.CalculateAveragePerRow(dataset);
+            //column = adapter.CalculateAveragePerColumn(dataset);
+            //for (int i = 0; i < row.Length; i++)
+            //{
+            //    Console.WriteLine(row[i] + "  ");
+            //}
+            //Console.WriteLine("\n");
+            //for (int i = 0; i < column.Length; i++)
+            //{
+            //    Console.WriteLine(column[i] + "  ");
+            //}
 
+            //3. zadatak
+            List<IRentable> rentableList = new List<IRentable>();
+            Book book1 = new Book("Don Quijote");
+            Video video1 = new Video("Dirty Grandpa");
+            rentableList.Add(book1);
+            rentableList.Add(video1);
+            RentingConsolePrinter printer = new RentingConsolePrinter();
+            printer.DisplayItems(rentableList);
+            printer.PrintTotalPrice(rentableList);
         }
     }
 }
