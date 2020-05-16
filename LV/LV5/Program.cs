53d466136b53d857ec35037e879897ea3a24128a
David Novak <davidnovak252@gmail.com>
2020-05-16 21:01:39 +0200

    Task4 LV5 Commit

--- LV/LV5/Program.cs
+++ LV/LV5/Program.cs
@@ -6,13 +6,18 @@ namespace LV5
     {
         static void Main(string[] args)
         {
-            IDataset dataPrint = new Dataset(@"D:/LV_DOOP/LV/LV5/test.csv");
-            DataConsolePrinter.dataPrint(dataPrint);
-            IDataset virtualProxyDataset = new VirtualProxyDataset(@"D:/LV_DOOP/LV/LV5/test.csv");
-            DataConsolePrinter.dataPrint(virtualProxyDataset);
-            User user = User.GenerateUser("User");
-            IDataset protectionProxyData = new ProtectionProxyDataset(user);
-            DataConsolePrinter.dataPrint(protectionProxyData);
+            //3. zadatak
+            //IDataset dataPrint = new Dataset(@"D:/LV_DOOP/LV/LV5/test.csv");
+            //DataConsolePrinter.dataPrint(dataPrint);
+            //IDataset virtualProxyDataset = new VirtualProxyDataset(@"D:/LV_DOOP/LV/LV5/test.csv");
+            //DataConsolePrinter.dataPrint(virtualProxyDataset);
+            //User user = User.GenerateUser("User");
+            //IDataset protectionProxyData = new ProtectionProxyDataset(user);
+            //DataConsolePrinter.dataPrint(protectionProxyData);
+
+            ITheme myCustomTheme = new Task4Theme();
+            ReminderNote reminderNote = new ReminderNote("My custom theme", myCustomTheme);
+            reminderNote.Show();
 
         }
     }
---
+++ LV/LV5/Task4Theme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class Task4Theme : ITheme
+    {
+        public void SetBackgroundColor()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+        public void SetFontColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        public string GetHeader(int width)
+        {
+            return new string('+', width);
+        }
+        public string GetFooter(int width)
+        {
+            return new string('_', width);
+        }
+
+    }
+}
+
