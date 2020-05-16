f7babd7eb49435cb52aaa2aed747ad4efeb4f91c
David Novak <davidnovak252@gmail.com>
2020-05-16 19:56:56 +0200

    Task1 LV5 Commit

---
+++ LV/LV5/IShipable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    interface IShipable
+    {
+        double Price { get; }
+        double Weight { get; }
+        string Description (int depth = 0);
+    }
+
+}
---
+++ LV/LV5/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class Product : IShipable
+    {
+        private double price;
+        public double weight;
+        private string description;
+        public Product(string description, double price) 
+        { 
+            this.description = description; 
+            this.price = price; 
+        }
+        public double Price { get { return this.price; } }
+
+        public double Weight { get { return this.weight; } }
+
+        public string Description(int depth = 0)
+        {
+            return new string(' ', depth) + this.description;}
+}
+    }
+}
