3b72cff6eabcf3bdab935f93c9f06151c0150967
David Novak <davidnovak252@gmail.com>
2020-05-16 20:06:04 +0200

    Task2 LV5 Commit

---
+++ LV/LV5/ShippingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class ShippingService 
+    {
+        private double weightPrice;
+        public ShippingService(double weightPrice) { this.weightPrice = weightPrice; }
+        public double shippingPrice (IShipable item)
+        {
+            double calculatedPrice = 0;
+            calculatedPrice = item.Weight * weightPrice;
+            return calculatedPrice;
+        }
+    }
+}
