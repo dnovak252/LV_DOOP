6871e7fb1f1f98ac8657d988844386b752e1208e
David Novak <davidnovak252@gmail.com>
2020-05-16 21:32:37 +0200

    Task6 LV5 Commit

---
+++ LV/LV5/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class BubbleSort : SortStrategy
+    {
+        public override void Sort(double[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j]>array[j + 1])
+                    {
+                        Swap (ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
