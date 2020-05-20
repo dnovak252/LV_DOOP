90ab891f9b0c36f25d52dc1abadfe2b76cb97b2b
David Novak <davidnovak252@gmail.com>
2020-05-20 13:12:02 +0200

    Task1 LV6 Commit

---
+++ LV/LV6/IAbstractIterator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV6
+{
+    interface IAbstractIterator
+    {
+        Note First(); Note Next(); 
+        bool IsDone { get; }
+        Note Current { get; }
+    }
+}
---
+++ LV/LV6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note note1 = new Note("First Note.", "I am note number one.");
+            Note note2 = new Note("Second Note", "I am note number two.");
+            Note note3 = new Note("Third Note", "I am note number three.");
+            Notebook notebook = new Notebook();
+            notebook.AddNote(note1);
+            notebook.AddNote(note2);
+            notebook.AddNote(note3);
+            IAbstractIterator iterator = notebook.GetIterator();
+            for (int i = 0; i < notebook.Count; i++)
+            {
+                iterator.Current.Show();  
+                iterator.Next();
+            }
+        }
+    }
+}
